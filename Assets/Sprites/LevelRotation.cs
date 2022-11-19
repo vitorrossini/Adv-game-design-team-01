61d4e1485ba144c96866ffdc91222924cca258ee
Vitor Rossini <vitorrossini@live.com>
2022-11-19 12:13:10 +0100

    rotation

---
+++ Assets/Sprites/LevelRotation.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelRotation : MonoBehaviour
+{
+    public float degreesPerSecond = 30;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.B))
+        {
+            RotateLevel();
+        }
+    }
+
+    public void RotateLevel()
+    {
+        transform.Rotate(new Vector3 (0,0,degreesPerSecond) * Time.deltaTime);
+    }
+}
