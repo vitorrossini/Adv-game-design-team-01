5e373ad7042204a0ccc7554bb6718491e165f14f
Vitor Rossini <vitorrossini@live.com>
2022-11-29 12:36:02 +0100

    guitar attempt

--- Assets/Scripts/3c/Inputs.cs
+++ Assets/Scripts/3c/Inputs.cs
@@ -5,19 +5,33 @@ public class Inputs : MonoBehaviour
     public ParametersSetByName music;
     [SerializeField] private GameObject wavePrefab;
     [SerializeField] private Transform playerPosition;
+    public float timer;
 
     void Update()
     {
         if (Input.GetButtonDown("Guitar"))
         {
             
-            music.UseGuitar1();
+            PlayGuitar();
             GameObject newWave = Instantiate(wavePrefab);
             newWave.transform.position = playerPosition.position;
         }
-        else
+        
+    }
+
+    public void PlayGuitar()
+    {
+        timer += Time.deltaTime;
+        if (timer <= 2f)
+        {
+            music.UseGuitar1();
+        }
+
+        if (timer > 2f)
         {
             music.UseGuitar2();
+            timer = 0;
         }
     }
+
 }
